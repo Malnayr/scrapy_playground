d5bf08e84869b9ca6c096870ff290c91778e0278
Ryan Lam <lam4374@saskpolytech.ca>
2017-04-25 10:59:03 -0600

    Scraped data into a json file and displayed json file to webpage

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -4,33 +4,35 @@ class QuotesSpider(scrapy.Spider):
     #Used to identify the spider - Can't use the same name for different spiders
     name = "quotes"
     start_urls = [
-        'http://quotes.toscrape.com/page/1/',
-        'http://quotes.toscrape.com/page/2/'
+        'http://quotes.toscrape.com/page/1/'
     ]
-
-    #Do not need this anymore - Since we will be using a shortcut to the start_requests method
-    # #Purpose: Return a iterable which the spider will crawl from
-    # def start_requests(self):
-    #     urls = [
-    #         'http://quotes.toscrape.com/page/1/',
-    #         'http://quotes.toscrape.com/page/2/'
-    #     ]
-    #     for url in urls:
-    #         #Returns a generator - A iterable/list of items from the current URL
-    #         yield scrapy.Request(url=url, callback=self.parse)
 
     #Purpose: Holds page content
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        #Make file name
-        filename = 'quotes-%s.html' % page
-        #Open file to write into
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        for quote in response.css('div.quote'):
+            yield {
+                'text' : quote.css('span.text::text').extract_first(),
+                'author' : quote.css('small.author::text').extract_first(),
+                'tags': quote.css('div.tags a.tag::text').extract()
+            }
+
+        # page = response.url.split("/")[-2]
+        # filename = 'quotes-%s.html' % page
+        # with open(filename, 'wb') as f:
+        #     f.write(response.body)
+
+        #Check if there is another page to move to - Previously we had a hardcoded pages in start_urls
+        next_page = response.css('li.next a::attr(href)').extract_first()
+        if next_page is not None:
+            #build a full absolute url using urljoin()
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
 
 #Run "scrapy crawl quotes"
 
 #Using Scrappy Shell:
     #Run "scrapy shell 'http://quotes.toscrape.com/page/1/'
+
+#scrapy crawl quotes -o quotes.json
+#scrapy crawl quotes -o quotes.jl
 
